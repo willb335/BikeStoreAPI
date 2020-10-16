249662b39faa09c1b21dc7becf7c9219e5fee7e8
willb335 <wbashelor@gmail.com>
2020-10-16 13:29:01 -0400

    Runs succesfully

--- SecureClient/Program.cs
+++ SecureClient/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace SecureClient
 {
@@ -6,9 +11,69 @@ namespace SecureClient
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Making the call...");
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task RunAsync()
+        {
             AuthConfig config = AuthConfig.ReadFromJsonFile("appsettings.json");
 
-            Console.WriteLine($"Authority: {config.Authority}");
+            IConfidentialClientApplication app;
+
+            app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                .WithClientSecret(config.ClientSecret)
+                .WithAuthority(new Uri(config.Authority))
+                .Build();
+
+            string[] ResourceIds = new string[] { config.ResourceID };
+
+            AuthenticationResult result = null;
+            try
+            {
+                result = await app.AcquireTokenForClient(ResourceIds).ExecuteAsync();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Token acquired \n");
+                Console.WriteLine(result.AccessToken);
+                Console.ResetColor();
+            }
+            catch (MsalClientException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+
+            if (!string.IsNullOrEmpty(result.AccessToken))
+            {
+                var httpClient = new HttpClient();
+                var defaultRequestHeaders = httpClient.DefaultRequestHeaders;
+
+                if (defaultRequestHeaders.Accept == null ||
+                   !defaultRequestHeaders.Accept.Any(m => m.MediaType == "application/json"))
+                {
+                    httpClient.DefaultRequestHeaders.Accept.Add(new
+                      MediaTypeWithQualityHeaderValue("application/json"));
+                }
+                defaultRequestHeaders.Authorization =
+                  new AuthenticationHeaderValue("bearer", result.AccessToken);
+
+                HttpResponseMessage response = await httpClient.GetAsync(config.BaseAddress);
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    string json = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(json);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Failed to call the Web Api: {response.StatusCode}");
+                    string content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Content: {content}");
+                }
+                Console.ResetColor();
+            }
         }
     }
 }
